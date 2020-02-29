5276a15a496b9304973cf8b8f9c8ac6b2ef1f35c
Ksuetech <arriolks01@mail.buffalostate.edu>
2020-02-29 01:41:58 -0500

    Add files via upload
    Testing the histogram using the percentage of each stock.

---
+++ StockAnalysis_Percent.r
@@ -0,0 +1,64 @@
+> summary(Dividendyieldd)
+Error in summary(Dividendyieldd) : object 'Dividendyieldd' not found
+> summary (Dividendyieldd)
+Error in summary(Dividendyieldd) : object 'Dividendyieldd' not found
+> attach (Stock.and.divdiends.according.to.STASH.)
+> summary(Dividendyieldd)
+   Min. 1st Qu.  Median    Mean 3rd Qu.    Max.    NA's 
+  0.000   0.460   1.610   1.995   3.010  14.540       1 
+> Stock.and.divdiends.according.to.STASH.<-read.csv("~Stock.and.divdiends.according.to.STASH.")
+Error in file(file, "rt") : cannot open the connection
+In addition: Warning message:
+In file(file, "rt") :
+  cannot open file '~Stock.and.divdiends.according.to.STASH.': No such file or directory
+> View (Stock.and.divdiends.according.to.STASH.)
+> 
+> summary (Dividendyieldd)
+   Min. 1st Qu.  Median    Mean 3rd Qu.    Max.    NA's 
+  0.000   0.460   1.610   1.995   3.010  14.540       1 
+> 
+> 
+> attach (Stock.and.divdiends.according.to.STASH.)
+The following objects are masked from Stock.and.divdiends.according.to.STASH. (pos = 3):
+
+    Company..Stock.name, Dividendyieldd, Dividendyieldp, Last.Price,
+    Ticker
+
+> sum (Dividendyieldd)
+[1] NA
+> mean(Dividendyieldd)
+[1] NA
+> summary(Dividendyieldp)
+   Min. 1st Qu.  Median    Mean 3rd Qu.    Max.    NA's 
+0.00000 0.00700 0.02000 0.02167 0.03100 0.11300       1 
+> hist(Dividendyieldd)
+> Stock.and.divdiends.according.to.STASH.<-read.csv("~Stock.and.divdiends.according.to.STASH.")
+Error in file(file, "rt") : cannot open the connection
+In addition: Warning message:
+In file(file, "rt") :
+  cannot open file '~Stock.and.divdiends.according.to.STASH.': No such file or directory
+> View (Stock.and.divdiends.according.to.STASH.)
+> 
+> summary (Dividendyieldd)
+   Min. 1st Qu.  Median    Mean 3rd Qu.    Max.    NA's 
+  0.000   0.460   1.610   1.995   3.010  14.540       1 
+> 
+> 
+> attach (Stock.and.divdiends.according.to.STASH.)
+The following objects are masked from Stock.and.divdiends.according.to.STASH. (pos = 3):
+
+    Company..Stock.name, Dividendyieldd, Dividendyieldp, Last.Price,
+    Ticker
+
+The following objects are masked from Stock.and.divdiends.according.to.STASH. (pos = 4):
+
+    Company..Stock.name, Dividendyieldd, Dividendyieldp, Last.Price,
+    Ticker
+
+> hist(Dividendyieldp)
+> hist(Dividendyieldd)
+> sd (Dividendyieldp)
+[1] NA
+> median(Dividendyieldd)
+[1] NA
+> hist(Dividendyieldp)
